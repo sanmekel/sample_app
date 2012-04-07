1d9b72196d85c40b7ae9ebf81351756910dac3d3
memnas <noypi2k3@yahoo.com>
2012-04-06 17:31:58 -0700

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -47,8 +47,21 @@ describe "Authentication" do
   end
 
   describe "authorization" do
+
   	describe "for non-signed-in users" do
   	  let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
 
   	  describe "when attempting to visit a protected page" do
   	  	before do
@@ -82,6 +95,16 @@ describe "Authentication" do
   	  	  before { visit users_path }
   	  	  it { should have_selector('title', text: 'Sign in') }
   	  	end
+
+        describe "visiting the following pages" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
   	  end
 
       describe "in the Microposts controller" do
